f114d6df7d78f06e018c51abdb24051cbabc30fd
Hudson França <hudsonsilvares@gmail.com>
2020-05-23 13:33:30 -0300

    Readme created.

--- src/components/ImageZoom/ImageGallery.js
+++ src/components/ImageZoom/ImageGallery.js
@@ -4,9 +4,9 @@ import { Carousel } from "react-responsive-carousel";
 
 export default function ImagesGallery({ images }) {
   return (
-    <Carousel infiniteLoop useKeyboardArrows width={"80%"}>
+    <Carousel infiniteLoop useKeyboardArrows dynamicHeight width={"70%"}>
       {images &&
-        images.map(image => (
+        images.map((image) => (
           <div key={image.id}>
             <img src={image.image} alt="product" />
           </div>
